5322fbcb0ca34dd6791a8130f5ae8ec43a0ae520
Brandon Trumfio <brandon.trumfio@thirdwavellc.com>
2017-03-18 19:49:34 -0500

    commit

---
+++ wordpress/wp-content/themes/realtors-to-the-rescue-theme/events.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Events Page
+*/
+?>
+
+<?php get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+
+
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
